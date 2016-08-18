3d3cb46552feb3ffd8eef328b963b2bf7075635a
MadRedis <pleozio@gmail.com>
2016-08-18 19:44:52 +0700

    s

---
+++ TextGame/Testing.java
@@ -0,0 +1,9 @@
+import java.util.Random;
+public class Testing
+{
+    public static void main(String[] args) {
+        Names name = new Names();
+        String itemName = name.getName();
+        System.out.println(itemName);
+    }
+}
